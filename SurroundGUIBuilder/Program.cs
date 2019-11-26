6dda3a0c44986f58e86a3556f7f89ad14c0b1032
James Bearss <james.bearss@outlook.com>
2019-11-26 16:57:23 -0500

    Added new project

---
+++ SurroundGUIBuilder/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SurroundGUIBuilder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
